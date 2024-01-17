6ec739866eeaed3e1e5a14415cd8286383355393
Paul Leenheer <bfound2@iMac-van-Paul-2.local>
2024-01-17 21:50:17 +0100

    Removed nextgenimages module.Must be installed via composer!

--- app/code/Yireo/NextGenImages/Exception/ConvertorException.php
+++
@@ -1,9 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace Yireo\NextGenImages\Exception;
-
-use Exception;
-
-class ConvertorException extends Exception
-{
-}
--- app/code/Yireo/NextGenImages/registration.php
+++
@@ -1,9 +0,0 @@
-<?php declare(strict_types=1);
-
-use Magento\Framework\Component\ComponentRegistrar;
-
-ComponentRegistrar::register(
-    ComponentRegistrar::MODULE,
-    'Yireo_NextGenImages',
-    __DIR__
-);
