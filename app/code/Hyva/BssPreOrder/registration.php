5c918bd03472962f35907eea7dce855c1708056e
Paul Leenheer <paul@b-found.nl>
2024-06-11 15:54:41 +0200

    Fixing #12 - Adding Hyva Compatibility module in local code pool

---
+++ app/code/Hyva/BssPreOrder/registration.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * BSS Commerce Co.
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the EULA
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://bsscommerce.com/Bss-Commerce-License.txt
+ *
+ * @category   BSS
+ * @package    Hyva_BssPreOrder
+ * @author     Extension Team
+ * @copyright  Copyright (c) 2022 BSS Commerce Co. ( http://bsscommerce.com )
+ * @license    http://bsscommerce.com/Bss-Commerce-License.txt
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Hyva_BssPreOrder',
+    __DIR__
+);
