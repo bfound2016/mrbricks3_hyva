83e4340a6592762c21a05d0eaa1dcf11363f6fad
Paul Leenheer <paul@b-found.nl>
2024-06-18 16:08:25 +0200

    Fixing #18 - Removing 2XL breakpoint thus creating new max-width of 1280 to container class

--- app/design/frontend/BFD/Hyva_Hypernode/web/tailwind/tailwind.config.js
+++ app/design/frontend/BFD/Hyva_Hypernode/web/tailwind/tailwind.config.js
@@ -16,10 +16,8 @@ module.exports = hyvaModules.mergeTailwindConfig({
         // => @media (min-width: 768px) { ... }
         'lg': '1024px',
         // => @media (min-width: 1024px) { ... }
-        'xl': '1280px',
+        'xl': '1280px'
         // => @media (min-width: 1280px) { ... }
-        '2xl': '1536px' // => @media (min-width: 1536px) { ... }
-
       },
       fontFamily: {
         sans: ["Poppins", "Segoe UI", "Helvetica Neue", "Arial", "sans-serif"]
