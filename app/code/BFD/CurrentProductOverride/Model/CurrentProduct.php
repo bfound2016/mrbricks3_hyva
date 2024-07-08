096fd31723b32e6cf42ad2a1b6561d54fdd9039d
Paul Leenheer <paul@b-found.nl>
2024-07-08 09:53:44 +0200

    Fixing #14 - Limit review rendering to 5 then add a "View All" button

---
+++ app/code/BFD/CurrentProductOverride/Model/CurrentProduct.php
@@ -0,0 +1,36 @@
+<?php
+namespace BFD_HyvaOverrides\CustomModule\Model;
+
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Store\Model\ScopeInterface;
+
+class CurrentProduct extends \Hyva\Theme\ViewModel\CurrentProduct
+{
+    /**
+     * @var ScopeConfigInterface
+     */
+    private $scopeConfig;
+
+    /**
+     * @param ScopeConfigInterface $scopeConfig
+     */
+    public function __construct(
+        ScopeConfigInterface $scopeConfig
+    ) {
+        $this->scopeConfig = $scopeConfig;
+        parent::__construct();
+    }
+
+    /**
+     * Get cutoff time from TIG PostNL module
+     *
+     * @return string
+     */
+    public function getCutoffTime()
+    {
+        // The path where cutoff time is stored in the store configuration
+        $path = 'tig_option_postnl/general/cutoff_time';
+
+        return $this->scopeConfig->getValue($path, ScopeInterface::SCOPE_STORE);
+    }
+}
